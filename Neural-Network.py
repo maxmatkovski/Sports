3da3d77a0859b1aba8ff9c688cc49eb33bdc1b8c
Max <74901101+datadrivenmax@users.noreply.github.com>
2023-06-02 02:11:12 -0700

    code copied. need to learn theory more.

--- Neural-Network.py
+++ Neural-Network.py
@@ -26,4 +26,49 @@ class NeuralNetwork:
         return self.a2
     
     def backward(self, X, y, learning_rate):
+        m = X.shape[0]  # Number of samples
         
+        # Compute gradients
+        dZ2 = self.a2 - y
+        dW2 = np.dot(self.a1.T, dZ2) / m
+        db2 = np.sum(dZ2, axis=0) / m
+        dZ1 = np.dot(dZ2, self.W2.T) * self.sigmoid_derivative(self.z1)
+        dW1 = np.dot(X.T, dZ1) / m
+        db1 = np.sum(dZ1, axis=0) / m
+        
+        # Update weights and biases
+        self.W2 -= learning_rate * dW2
+        self.b2 -= learning_rate * db2
+        self.W1 -= learning_rate * dW1
+        self.b1 -= learning_rate * db1
+    
+    def train(self, X, y, epochs, learning_rate):
+        for epoch in range(epochs):
+            # Forward propagation
+            outputs = self.forward(X)
+            
+            # Backward propagation
+            self.backward(X, y, learning_rate)
+            
+            # Compute loss (optional)
+            loss = self.calculate_loss(y, outputs)
+            print(f"Epoch {epoch+1}/{epochs} - Loss: {loss}")
+    
+    def predict(self, X):
+        # Forward propagation
+        outputs = self.forward(X)
+        predictions = np.round(outputs)
+        
+        return predictions
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def sigmoid_derivative(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
+    
+    def calculate_loss(self, y_true, y_pred):
+        epsilon = 1e-7  # Small value to avoid division by zero
+        loss = -np.mean(y_true * np.log(y_pred + epsilon) + (1 - y_true) * np.log(1 - y_pred + epsilon))
+        
+        return loss
